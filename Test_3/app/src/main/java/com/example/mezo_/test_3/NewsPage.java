dde50aa0c1f9b6a7b467f01d37ccdb112f18cf9c
Moataz00 <32262369+Moataz00@users.noreply.github.com>
2017-09-25 13:57:42 -0700

    android
    android desc

---
+++ Test_3/app/src/main/java/com/example/mezo_/test_3/NewsPage.java
@@ -0,0 +1,22 @@
+package com.example.mezo_.test_3;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by mezo_ on 9/7/2017.
+ */
+
+public class NewsPage extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        
+        return inflater.inflate(R.layout.news_screen, container, false);
+    }
+}
