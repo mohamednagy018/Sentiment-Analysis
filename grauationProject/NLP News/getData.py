c25ebdb55c736ee62d549ce6357bbdcdda458848
MohamedAdel100 <31737995+MohamedAdel100@users.noreply.github.com>
2017-10-04 20:16:04 +0200

    adel

--- grauationProject/NLP News/getData.py
+++ grauationProject/NLP News/getData.py
@@ -1,5 +1,8 @@
 import feedparser as fd
-
+import requests
+import re
+from html.parser import HTMLParser
+from difflib import SequenceMatcher
 
 url = "http://feeds.bbci.co.uk/arabic/world/rss.xml" #BBC
 url2 = "https://aawsat.com/feed/news" #Awsaat
@@ -34,7 +37,73 @@ for item in items:
         titles.append(item[x]["title"])
 
 
-print(summaries[0])
-print(links[0])
-print(titles[0])
+#print(summaries[1])
+#print(links[1])
+#print(titles[1])
+#--------------------------------------------------------------------------------------------------
+
+
+def similar(a, b):
+    return SequenceMatcher(None, a, b).ratio()
+
+
+def Get_HTML_Script(link):
+   HTML_script =requests.get(link)
+   Script=HTML_script.text
+   return Script
+
+
+
+def Get_Paragraph(HTML_script):
+    Paragraph_array = []
+    for x in HTML_script.split('\n'):
+        search_obj = re.search(r'<p(.)*>(.)*</p>'  , x, flags=0)
+        if search_obj:
+            Paragraph_array.append(search_obj.group())
+    return Paragraph_array
+class MyHTMLParser(HTMLParser):
+    check_type = ""
+    def handle_starttag(self, tag, attrs):
+        self.check_type=tag
+
+    def handle_data(self, data):
+       if len(Paragraph_array)>0:
+           if self.check_type=="p" or self.check_type=="a":
+             all_news.append(data)
+       else:
+            if self.check_type!="a":
+                all_news.append(data)
+       self.check_type=""
+
+
+for item_news in range(0,len(links)):
+    print(summaries[item_news])
+    print(links[item_news])
+    print(titles[item_news])
+    print("####################################################################################")
+    script=Get_HTML_Script(links[item_news])
+    #print(script)
+    Paragraph_array=Get_Paragraph(script)
+    check_type=""
+    all_news=[]
+    parser = MyHTMLParser()
+    if len(Paragraph_array)>0:
+     for x in Paragraph_array:
+      parser.feed(x)
+    else:
+        parser.feed(script)
+    news=""
+    index=0
+    for x in all_news:
+        #print(similar(x,summaries[item_news]))
+        if similar(x,summaries[item_news])>=.6:
+            index=all_news.index(x)
+            break
+    while index!=len(all_news):
+        print(all_news[index])
+        news+=all_news[index]
+        index=index+1
+    print("-------------------------------------------------------------")
+   #print(news)
+
  
