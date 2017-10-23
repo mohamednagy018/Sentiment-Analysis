c975ed2a3d81390c12a95189323bc1a77d49c4a8
mohamednagy018 <mohamednagy018@gmail.com>
2017-10-23 17:32:41 +0200

    test

--- grauationProject/NLP News/mostTrend.py
+++
@@ -1,1 +0,0 @@
-import Preprocessing as p
--- grauationProject/NLP News/recommederSystem.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-import numpy as np
-import python.labr as l
-
-
-filename = 'data/labr_data/reviews.tsv'
-file=l.LABR.read_review_file(l,filename)
-
-print(file[0])
